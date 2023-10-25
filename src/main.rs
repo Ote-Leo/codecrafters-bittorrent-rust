e9eb5bc615adebe056eb865fb24c6c3b34987715
Ote Leo <ote.leo73@gmail.com>
2023-10-25 09:33:23 +0300

    passing first stage

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,8 @@ fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
         // Example: "5:hello" -> "hello"
         let colon_index = encoded_value.find(':').unwrap();
         let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
+        let length = number_string.parse::<i64>().unwrap();
+        let string = &encoded_value[colon_index + 1..colon_index + 1 + length as usize];
         return serde_json::Value::String(string.to_string());
     } else {
         panic!("Unhandled encoded value: {}", encoded_value)
@@ -26,12 +26,12 @@ fn main() {
 
     if command == "decode" {
         // You can use print statements as follows for debugging, they'll be visible when running tests.
-        println!("Logs from your program will appear here!");
+        // println!("Logs from your program will appear here!");
 
         // Uncomment this block to pass the first stage
-        // let encoded_value = &args[2];
-        // let decoded_value = decode_bencoded_value(encoded_value);
-        // println!("{}", decoded_value.to_string());
+        let encoded_value = &args[2];
+        let decoded_value = decode_bencoded_value(encoded_value);
+        println!("{}", decoded_value.to_string());
     } else {
         println!("unknown command: {}", args[1])
     }
